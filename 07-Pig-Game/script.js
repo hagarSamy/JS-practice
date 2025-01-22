c3334bf562892902236a09c30fd42dd32fbf5b1a
Hagar Samy <hs1800@fayoum.edu.eg>
2025-01-22 23:18:42 +0200

    adding rolling dice func

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -4,8 +4,25 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Resetting initial state
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+//diceROll func
+const roll = function() {
+  // gen random dice num
+  const dice = Math.trunc((Math.random() * 6) + 1);
+  console.log(dice);
+  // display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // check if to shift player
+};
+
+// rolling the dice
+btnRoll.addEventListener("click", roll);
