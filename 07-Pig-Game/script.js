eaff53d07567794344ba5b4c8b33ae6cc2143a58
Hagar Samy <hs1800@fayoum.edu.eg>
2025-01-24 22:04:21 +0200

    refining the reset function

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -12,20 +12,25 @@ const btnHold = document.querySelector('.btn--hold');
 const player0El = document.querySelector(".player--0");
 const player1El = document.querySelector(".player--1");
 
+let currentScore, playing, activePlayer, totalScores;
 // Resetting initial state
 const reset = function() {
+  totalScores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
   score0El.textContent = 0;
   score1El.textContent = 0;
+
   diceEl.classList.add('hidden');
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player-w-inner");
 };
 
 reset();
-
-// initial state
-const totalScores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
 
 // switch player func
 const switchPlayer = function() {
@@ -78,7 +83,4 @@ btnHold.addEventListener("click", function() {
 }});
 
 // resetting the game
-btnNew.addEventListener("click", function() {
-  currentScore = 0;
-  reset();
-})
+btnNew.addEventListener("click", reset);
