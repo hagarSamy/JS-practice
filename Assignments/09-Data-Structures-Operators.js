96189532745b3e5f60978088c0f80d92b1a3e653
Hagar Samy <hs1800@fayoum.edu.eg>
2025-02-17 23:21:22 +0200

    Nullish Coalescing Operator (??)

--- Assignments/09-Data-Structures-Operators.js
+++ Assignments/09-Data-Structures-Operators.js
@@ -326,3 +326,12 @@ console.log(hasExamplesInJava(books[1]));
 for (let i = 0; i < books.length; i++) {
     books[i].onlineContent && console.log(`"${books[i].title}" provides online content`);
 }
+
+////////////////////////////The Nullish Coalescing Operator (??)﻿
+// 6.1﻿ There are objects in the books array that don't have the onlineContent property at all. 
+// Loop over the books array, and log a string to the console in this format: "${title}" provides
+//  no data about its online content.
+for (let i = 0; i < books.length; i++) {
+    // logs result only if val != null || undefined || false
+    books[i].onlineContent && console.log(`"${books[i].title}" provides no data about its online content`);
+}
