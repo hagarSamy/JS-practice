36777192f3a238fedb8b3159fdf6349e902ba6b8
Hagar Samy <hs1800@fayoum.edu.eg>
2025-02-28 19:43:31 +0200

    optional chaining

--- Assignments/09-Data-Structures-Operators.js
+++ Assignments/09-Data-Structures-Operators.js
@@ -417,3 +417,14 @@ const newBook2 = {
   pages,
 };
 console.log(newBook2);
+
+
+////////////////Optional Chaining (?.)
+//10.1 Write a function called getFirstKeyword that takes the book object as an argument. This function should return the first keyword from the book's keywords property 
+// (array) or undefined (if the keywords property doesn't exist). It shouldn't throw an error. Use optional chaining for that.
+
+getFirstKeyword = function(book) {
+  return book.keywords?.[0] || "undefined";
+}
+console.log(getFirstKeyword(books[0]));
+console.log(getFirstKeyword(books[newBook2]));
