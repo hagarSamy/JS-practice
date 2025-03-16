4de6d7770b3eaa8017ad9cd3e409d421dcf3153a
hagarSamy <hs1800@fayoum.edu.eg>
2025-03-16 15:38:28 +0200

    maps

--- Assignments/09-Data-Structures-Operators.js
+++ Assignments/09-Data-Structures-Operators.js
@@ -487,3 +487,35 @@ console.log(uniqueKeywordsArr);
 // 12.6 Delete all items from the uniqueKeywords set.
 uniqueKeywords.clear();
 console.log(uniqueKeywords);
+
+//////////////////////////////// Maps: Fundamentals﻿
+// 13.1﻿ Create a new book, but this time, as a Map. Assign it to the bookMap variable. Use this array 
+// as initial data: [['title', 'Clean Code'], ['author', 'Robert C. Martin']]
+
+const bookMap = new Map([['title', 'Clean Code'], ['author', 'Robert C. Martin']]);
+
+// 13.2﻿ Set a new key in bookMap called pages, and assign it with a number 464.
+bookMap.set('pages', 464);
+
+// 13.3﻿ Get the title and author values from bookMap, and log to the console a string formatted like that: "${title} by ${author}".
+console.log(`${bookMap.get('title')} by ${bookMap.get('author')}`);
+
+// 13.4﻿ Get the size of bookMap, and log it to the console.
+console.log(bookMap.size);
+
+// 13.5﻿ Check if bookMap has the author key. and if so, log "The author of the book is known" to the console.
+if (bookMap.has('author')) console.log("The author of the book is known");
+
+///////////////////////////////// Maps: Iteration﻿
+// 14.1﻿ Convert the first book object from the books array into a Map, and assign it to a firstBookMap variable.
+
+const firstBookMap = new Map(Object.entries(books[0]));
+
+// 14.2﻿ Use the for-of loop to iterate over firstBookMap, and log to the console keys that have numbers as values
+for(let [key, value] of firstBookMap) {
+  if (typeof(value) === 'number')
+  {
+    console.log(key);
+  }
+}
+console.log(firstBookMap);
